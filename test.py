5454b53d810d566d01ec46705d41824d2e454f26
Chris Boettner <boettnec@gmail.com>
2023-01-23 21:12:23 +0100

    first steps

---
+++ test.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 23 15:24:09 2023
+
+@author: chris
+"""
+
+import os
+import random
+import time
+import chess
+import chess.engine
+
+# engine = chess.engine.SimpleEngine.popen_uci(r"stockfish/stockfish_15.1_x64_bmi2")
+# engine2 = chess.engine.SimpleEngine.popen_uci(r"stockfish/stockfish_15.1_x64_bmi2")
+# engine2.configure({"Skill Level": 10})
+
+# board = chess.Board()
+
+# turn = 0
+# while not board.is_game_over():
+#     if turn//2:
+#         result = engine.play(board, chess.engine.Limit(time=0.1))
+#     else:
+#         result = engine2.play(board, chess.engine.Limit(time=0.1))
+#     board.push(result.move)
+#     print(board)
+#     print()
+
+# engine.quit()
+
+
+class Game(object):
+    def __init__(self, engine:str='stockfish',
+                       player_colour:str='white',
+                       engine_limit:int=0.1,
+                       engine_skill_level:int = 10) -> None:
+        
+        # load engine
+        if engine == 'stockfish':
+            self.engine = (chess.engine.SimpleEngine
+                            .popen_uci(r"stockfish/stockfish_15.1_x64_bmi2"))
+        # set attributes
+        if player_colour.casefold() in ['white', 'black']:
+            self.player_colour = player_colour
+        else:
+            raise ValueError('player_colour must be white or black.')
+        self.board         = chess.Board()
+        self.engine_limit  = engine_limit
+        # configure engine
+        self.engine.configure({"Skill Level": engine_skill_level})
+        
+        
+    def start_game(self) -> None:
+        self.reset_game()
+        self._game()
+        
+    def continue_game(self) -> None:
+        raise NotImplementedError('Does not quite work yet, since we do not '
+                                  'save whos turn it is.')
+        self._game()
+        
+    def reset_game(self) -> None:
+        self.board.reset()
+        
+    def _game(self) -> None:
+        # player order
+        turns = ([self._player_turn, self._engine_turn]
+                 if self.player_colour == 'white' 
+                 else [self._engine_turn, self._player_turn])
+        
+        # play
+        while not self.board.is_game_over():
+            for turn in turns:
+                move = turn()
+                if isinstance(move, chess.Move):
+                    self.board.push(move)
+                    if self.board.is_game_over():
+                        break
+                else:
+                    print('Qutting game.')
+                    return
+                
+        # print outcome
+        outcome = self.board.outcome()
+        if outcome.winner is None: 
+            print(f'Winner: Draw - {outcome.termination.name}')
+        elif outcome.winner is False:
+            print('Winner: Black')
+        else:
+            print('Winner: White')
+        
+    def _engine_turn(self) -> chess.Move:
+        engine_move = self.engine.play(self.board, 
+                            chess.engine.Limit(time=self.engine_limit)).move
+        return(engine_move)
+        
+    def _player_turn(self) -> chess.Move | None:
+        # list legal moves
+        moves      = list(self.board.legal_moves)
+        moves_dict = {str((1+n)) : moves[n] for n in range(len(moves))} 
+        
+        # print moves
+        for key, m in moves_dict.items():
+            key       = key if len(str(key))==2 else (' '+str(key))
+            move_name = m.uci()[:2] + ' -> ' + m.uci()[2:]
+            print(f'{key}    :   {move_name}')
+        print('quit  :       quit\n')
+        print(self.board)
+        print()
+        
+        # input prompt prompt
+        while True:
+            player_move = input('Choose your move: ')
+            if player_move in list(moves_dict.keys()):
+                return(moves_dict[player_move])
+            elif player_move in ['q', 'quit']:
+                break                
+            else:
+                print('  Input must be correspond to a move value or quit.'
+                      ' Try again.')
