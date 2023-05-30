1729e6511829da80f5a271b770454491bf1cbb5f
Chris Boettner <boettnec@gmail.com>
2023-05-30 23:13:32 +0200

    stockfish self-play works now, I think

--- mcts.py
+++ mcts.py
@@ -5,14 +5,14 @@ Created on Mon May 29 08:22:40 2023
 
 @author: chris
 """
-import chess
 from chess import Board, Move
+import chess.engine
 from scipy.special import softmax   
 import numpy as np
-import random
 
 class Node:
-    def __init__(self, state:str, parent=None, policy_probability=1) -> None:
+    def __init__(self, state:str, parent=None, policy_probability=1, 
+                 training_node=False) -> None:
         self.state = state
         
         if self.state is not None:
@@ -32,7 +32,8 @@ class Node:
         self.policy_probability = policy_probability
         
         self.puct = None
-        self.calculate_puct()
+        if not training_node:
+            self.calculate_puct()
     
     def fill(self, action):
         self.make_board()
@@ -55,17 +56,19 @@ class Node:
     def check_game_over(self):
         self.game_over = self.board.is_game_over()
     
-    def expand(self):
-        policy = self.get_policy()
+    def expand(self, **kwargs):
+        policy = self.get_nn_policy()
         for i, move in enumerate(self.board.legal_moves):
             self.children[move.uci()] = Node(None, parent=self,
-                                             policy_probability=policy[i])            
+                                             policy_probability=policy[i],
+                                             **kwargs)
+        self.legal_actions = list(self.children.keys())    
         self.is_leaf = False
     
     def get_value(self):
         return np.random.uniform(-1,1)
     
-    def get_policy(self):
+    def get_nn_policy(self):
         pol = np.random.rand(len(list(self.board.legal_moves)))
         return pol/pol.sum()
     
@@ -102,12 +105,14 @@ class Node:
     
     
 class Game:
-    def __init__(self, engine='stockfish') -> None: 
-        self.root = Node(Board().epd())
+    def __init__(self, root_state = None) -> None: 
+        self.root_state = root_state
+        self.reset()
         
-        if engine == 'stockfish':
-            self.engine = (chess.engine.SimpleEngine
-                            .popen_uci(r"stockfish/stockfish_15.1_x64_bmi2"))
+        if root_state is None:
+            self.root = Node(Board().epd())
+        else:
+            self.root = Node(root_state)
         
     def play(self, **kwargs):
         game_states = []
@@ -121,7 +126,7 @@ class Game:
             if i%100 == 0:
                 print(i) 
             i += 1
-            _, node, policy = self.choose_move(node)
+            _, node, policy = self.choose_move(node, **kwargs)
             policies.append(policy)
             game_states.append(node.state)
             if node.game_over:
@@ -129,33 +134,32 @@ class Game:
         target_values = self.get_target_values(node, len(game_states))
         
         return game_states, policies, target_values 
+    
+    def reset(self):
+        if self.root_state is None:
+            self.root = Node(Board().epd())
+        else:
+            self.root = Node(self.root_state)
 
     def choose_move(self, node, mode='stochastic', **kwargs):
-        policy = self.get_policy_stockfish(node, **kwargs)
-        breakpoint()
-        if mode=='stochastic':
-            action, new_node = random.choices(list(node.children.items()), 
-                                            policy, k=1)[0]
+        policy = self.get_policy(node, **kwargs)
 
+        if mode=='stochastic':
+            action, new_node = np.random.choices(list(node.children.items()), 
+                                            p = policy)
         elif mode=='deterministic':
-            action, new_node = list(node.children.items())[np.argmax(policy)]            
+            action, new_node = list(node.children.items())[np.argmax(policy)]  
+        else:
+            return ValueError("mode not known.")    
+        
         return action, new_node, policy
             
     @staticmethod
     def get_policy(node, temperature=1, plays=25):
         for i in range(plays):
             game.selection(node)
-        policy = node.get_child_visits()**temperature
+        policy = np.power(node.get_child_visits(), 1/temperature)
         return policy
-    
-    def get_policy_stockfish(self, node, time_limit=.01):
-        scores = []
-        for move in node.board.legal_moves:
-            info = self.engine.analyse(node.board, 
-                                        chess.engine.Limit(time=time_limit),
-                                        root_moves=[move])
-            scores.append(info["score"].relative.score())
-        return softmax(scores)
     
     @staticmethod
     def get_target_values(node, number_of_moves):
@@ -206,7 +210,130 @@ class Game:
         if node!=self.root:
             node.calculate_puct()
         return -value
+    
+    
+class TrainingGame:
+    def __init__(self, root_state=None, engine='stockfish', engine_skill=18) -> None: 
+        self.root_state = root_state
+        self.reset()
+        
+        if engine == 'stockfish':
+            self.engine = (chess.engine.SimpleEngine
+                           .popen_uci(r"stockfish/stockfish_15.1_x64_bmi2"))
+            
+            match engine_skill:
+                case "random": 
+                    engine_skill = self.random_skill_level()
+                    engine_skill = (engine_skill, engine_skill)
+                case int():
+                    engine_skill = (engine_skill, engine_skill)
+                case (int(), int()): 
+                    pass
+                case ("random", int()):
+                    engine_skill = (self.random_skill_level(), engine_skill[1])
+                case ("random", int()):
+                    engine_skill = (engine_skill[0], self.random_skill_level())
+                case ("random", "random"):
+                    engine_skill = (self.random_skill_level(),
+                                    self.random_skill_level())
+                case _ :
+                    raise ValueError("engine_skill input not understood.")
+                    
+            self.engine_skill = engine_skill
+        
+    @staticmethod
+    def random_skill_level(power=-0.2):
+        levels = np.arange(1,21)
+        weights = np.abs((levels-np.mean(levels)))**power
+        return(np.random.choice(levels, p = weights/sum(weights)))   
+     
+    def play(self, root_state=None, **kwargs):
+        game_states = []
+        policies    = []
+        
+        node = self.root
+        
+        game_states.append(node.state)
+        while True:
+            _, node, policy = self.choose_move(node, **kwargs)
+            policies.append(policy)
+            game_states.append(node.state)
+            if node.game_over:
+                break
+        target_values = self.get_target_values(node, len(game_states))
+        return game_states, policies, target_values 
+    
+    def reset(self):
+        self.move_counter = 0
+        
+        if self.root_state is None:
+            self.root = Node(Board().epd(), training_node=True)
+        else:
+            self.root = Node(self.root_state, training_node=True)
+
+    def choose_move(self, node, mode='stochastic', **kwargs):
+        node.expand(training_node=True)
+        policy = self.get_policy_stockfish(node, **kwargs)
+        
+        if mode=='stochastic':
+            action = np.random.choice(list(policy.keys()),p=list(policy.values()))
+        elif mode=='deterministic':
+            action = list(policy.keys())[np.argmax(list(policy.values()))]
+        else:
+            return ValueError("mode not known.")
+            
+        new_node = node.children[action]           
+        new_node.fill(action)
+        self.move_counter +=1
+        
+        return action, new_node, policy
+    
+    def get_policy_stockfish(self, node, time_limit=.01, temperature=1, 
+                             mate_score=int(1e+5)):
+        infos = self.engine.analyse(node.board, chess.engine.Limit(time=time_limit), 
+                                    multipv=500, 
+                                    options = {"Skill Level": 
+                                               self.engine_skill[self.move_counter%2]})
+
+        scores = [info["score"].relative.score(mate_score=mate_score) for info in infos]
+        scores = softmax(scores)**(1/temperature)
+        scores = scores/np.sum(scores)
+        
+        scores = {info["pv"][0].uci():score for info, score in zip(infos,scores)}
+        return scores
+    
+    @staticmethod
+    def get_target_values(node, number_of_moves):
+        last_turn = node.board.turn
+        winner    = node.board.outcome().winner
+        
+        if winner is None:
+            target_values = np.zeros(number_of_moves)
+        else:
+            target_values = np.empty((number_of_moves,)).astype(int)
+            if number_of_moves%2:
+                target_values[::2] = 1
+                target_values[1::2] = -1
+            else:
+                target_values[::2] = -1
+                target_values[1::2] = 1
+                
+            if last_turn!=winner:
+                target_values *= -1
+        
+        return target_values
          
 if __name__=='__main__':
-    game = Game()
-    a, b, c = game.play()
+    game = TrainingGame()
+    k = 0
+    for i in range(30):
+        a, b, c = game.play()
+        game.reset()
+        if sum(c)==0:
+            k +=1
+            
+    # Skill level might not work, since main difference in skill levels seems to be that
+    # it takes non-ideal moves as evaluated by analyse function, but we don't use the
+    # plays picked by the engine, so that will have no effect
+    
+    # probably better to increase temperature in order to increase randomness 
